87b537f30b6bd2ba09f9735a82c5f7c51f9b224a
harukodi <48220443+harukodi@users.noreply.github.com>
2022-09-21 16:34:36 +0200

    Update k3s-cluster-join.sh
    Disable traefik

--- k3s-cluster-join.sh
+++ k3s-cluster-join.sh
@@ -8,5 +8,5 @@ then
 echo "Token or IP was left empty try running the script again"
 exit 1
 else
-curl -sfL https://get.k3s.io | K3S_TOKEN=$SECRET sh -s - server --server https://$ipToMaster:6443
+curl -sfL https://get.k3s.io | K3S_TOKEN=$SECRET sh -s - server --server https://$ipToMaster:6443 --disable traefik
 fi
