7e8281ba8518a49f447b3f62b5b2a3e9d749d40b
harukodi <48220443+harukodi@users.noreply.github.com>
2022-09-21 16:33:58 +0200

    Update k3s-server-init.sh
    Disable traefik.

--- k3s-server-init.sh
+++ k3s-server-init.sh
@@ -6,7 +6,7 @@ then
 echo "token empty"
 exit 1
 else
-curl -sfL https://get.k3s.io | K3S_TOKEN=$SECRET sh -s - server --cluster-init
+curl -sfL https://get.k3s.io | K3S_TOKEN=$SECRET sh -s - server --cluster-init --disable traefik
 clear
 echo -e "Use this token to join your other nodes to this cluster \n$SECRET\n"
 echo -e "Use the local ip of this master node when running the cluster-join.sh \n"
