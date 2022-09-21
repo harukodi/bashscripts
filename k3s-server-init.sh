65150277f823508a2cffb915a7f58400f5eb5b43
harukodi <48220443+harukodi@users.noreply.github.com>
2022-09-21 22:27:23 +0200

    Update k3s-server-init.sh
    adding helm to the k3s-server-init.sh

--- k3s-server-init.sh
+++ k3s-server-init.sh
@@ -7,6 +7,11 @@ echo "token empty"
 exit 1
 else
 curl -sfL https://get.k3s.io | K3S_TOKEN=$SECRET sh -s - server --cluster-init --disable traefik
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update -y
+sudo apt-get install helm -y
 clear
 echo -e "Use this token to join your other nodes to this cluster \n$SECRET\n"
 echo -e "Use the local ip of this master node when running the cluster-join.sh \n"
