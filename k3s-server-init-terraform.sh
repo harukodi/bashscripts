cdaca5b71edec2ae8d5051fa1f4524e6725931a7
harukodi <48220443+harukodi@users.noreply.github.com>
2022-10-05 15:20:43 +0200

    Script for automation with terraform

---
+++ k3s-server-init-terraform.sh
@@ -0,0 +1,19 @@
+echo "enter your join token for the k3s cluster"
+SECRET=$1
+
+if [ -z "$SECRET" ]
+then
+echo "token empty"
+exit 1
+else
+curl -sfL https://get.k3s.io | K3S_TOKEN=$SECRET sh -s - server --cluster-init --disable traefik
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update -y
+sudo apt-get install helm -y
+clear
+echo -e "Use this token to join your other nodes to this cluster \n$SECRET\n"
+echo -e "Use the local ip of this master node when running the cluster-join.sh \n"
+ip -br a
+fi
