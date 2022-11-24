d3d63cc99f934cf79f6d0e0f8641bc100233f24f
xia1997x <48220443+harukodi@users.noreply.github.com>
2022-11-24 19:18:39 +0100

    Update x-ui-docker-init.sh

--- x-ui-docker-init.sh
+++ x-ui-docker-init.sh
@@ -10,8 +10,8 @@ sudo mkdir -p /etc/x-ui/cert/
 
 #cp cert.pem and key.pem to /etc/x-ui/cert/
 openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem -subj "/C=/ST=/L=/O=/OU=/CN="
-cp key.pem /etc/x-ui/cert/
-cp cert.pem /etc/x-ui/cert/
+mv key.pem /etc/x-ui/cert/
+mv cert.pem /etc/x-ui/cert/
 
 #Creating the docker ct with x-ui
 docker run -itd --network=host -v /etc/x-ui/db/:/etc/x-ui/ -v /etc/x-ui/cert/:/root/cert/ --name x-ui --restart=unless-stopped enwaiax/x-ui:latest
