d23608bb62f75e7af459c162cdeebaf604d4235b
xia1997x <48220443+harukodi@users.noreply.github.com>
2022-11-24 18:44:08 +0100

    Script for x-ui with terraform

---
+++ x-ui-docker-init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Install docker
+sudo apt update
+sudo apt install docker.io -y
+
+#Creating the mountpath for the docker ct
+mkdir -p /etc/x-ui/{db,cert}
+
+#Cd to /etc/x-ui/cert to create the key.pem and cert.pem
+cd /etc/x-ui/cert
+openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem -subj "/C=/ST=/L=/O=/OU=/CN="
+
+#Creating the docker ct with x-ui
+docker run -itd --network=host -v /etc/x-ui/db/:/etc/x-ui/ -v /etc/x-ui/cert/:/root/cert/ --name x-ui --restart=unless-stopped enwaiax/x-ui:latest
