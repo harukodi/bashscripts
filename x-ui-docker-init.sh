b753a4839901c3a6ea486aa132e4c5bd40bce7db
xia1997x <48220443+harukodi@users.noreply.github.com>
2022-11-24 19:15:16 +0100

    Update x-ui-docker-init.sh

--- x-ui-docker-init.sh
+++ x-ui-docker-init.sh
@@ -5,8 +5,8 @@ sudo apt update
 sudo apt install docker.io -y
 
 #Creating the mountpath for the docker ct
-sudo -p mkdir /etc/x-ui/db/
-sudo -p mkdir /etc/x-ui/cert/
+sudo mkdir -p /etc/x-ui/db/
+sudo mkdir -p /etc/x-ui/cert/
 
 #cp cert.pem and key.pem to /etc/x-ui/cert/
 openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem -subj "/C=/ST=/L=/O=/OU=/CN="
