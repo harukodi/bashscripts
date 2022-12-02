54f599f9250deb187c3204dc12ffefeebf2069e8
xia1997x <48220443+harukodi@users.noreply.github.com>
2022-12-02 01:33:13 +0100

    Create install-openvpn-as.sh

---
+++ install-openvpn-as.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Script to install openvpn-as
+
+#Password variable for openvpn-as
+openvpn_as_password=$1
+
+#Install openvpn-as
+apt update 
+apt -y install ca-certificates wget net-tools gnupg
+wget https://as-repository.openvpn.net/as-repo-public.asc -qO /etc/apt/trusted.gpg.d/as-repository.asc
+echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/as-repository.asc] http://as-repository.openvpn.net/as/debian jammy main">/etc/apt/sources.list.d/openvpn-as-repo.list
+apt update
+apt -y install openvpn-as
+
+#Changing the default password for openvpn-as user
+sleep 5
+sacli --user openvpn --new_pass $openvpn_as_password SetLocalPassword
+
+#Clearing terminal
+clear
+
+#Echo username and password
+echo "Username: openvpn password: $openvpn_as_password"
