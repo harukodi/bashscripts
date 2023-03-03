2d36ea0266ccdacff9503e8f2687999b52d100a9
xia1997x <48220443+harukodi@users.noreply.github.com>
2023-03-03 22:30:06 +0100

    Update install-teleport.sh

--- install-teleport.sh
+++ install-teleport.sh
@@ -5,6 +5,9 @@ DOMAIN_NAME=$2
 
 if [ -z "$EMAIL" ] || [ -z "$DOMAIN_NAME" ]
 then
+echo "Example: ./install-teleport.sh example@example.com tele.example.com"
+exit 1
+else
 sudo curl https://apt.releases.teleport.dev/gpg \
 -o /usr/share/keyrings/teleport-archive-keyring.asc
 
@@ -18,6 +21,4 @@ sudo apt-get install teleport
 
 teleport configure --acme --acme-email=$EMAIL --cluster-name=$DOMAIN_NAME | \
 sudo tee /etc/teleport.yaml > /dev/null
-else
-echo "Example: ./install-teleport.sh example@example.com tele.example.com"
 fi
