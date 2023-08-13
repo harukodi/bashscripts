eb34da055f8e4aca6e5c8fe35e1368da4382b573
xia1997x <48220443+harukodi@users.noreply.github.com>
2023-08-13 23:04:38 +0200

    remove curl line from salt scripts

--- saltstack-minion-install.sh
+++ saltstack-minion-install.sh
@@ -6,8 +6,6 @@ saltstack_master_ip=$1
 
 
 # Add saltstack repo
-sudo curl -fsSL -o /etc/apt/keyrings/salt-archive-keyring-2023.gpg [https://repo.saltproject.io/salt/py3/ubuntu/22.04/amd64/SALT-PROJECT-GPG-PUBKEY-2023.gpg](https://repo.saltproject.io/salt/py3/ubuntu/22.04/amd64/SALT-PROJECT-GPG-PUBKEY-2023.gpg)
-
 echo "deb [signed-by=/etc/apt/keyrings/salt-archive-keyring-2023.gpg arch=amd64] [https://repo.saltproject.io/salt/py3/ubuntu/22.04/amd64/latest](https://repo.saltproject.io/salt/py3/ubuntu/22.04/amd64/latest) jammy main" | sudo tee /etc/apt/sources.list.d/salt.list
 
 
@@ -23,4 +21,3 @@ echo "master: $saltstack_master_ip" >> /etc/salt/minion.d/master.conf
 # Enable the salt-minion service
 sudo systemctl enable salt-minion
 sudo systemctl start salt-minion
-
