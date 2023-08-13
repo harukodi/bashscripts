d3950d6fc9c52b796070007e2f107a0fa70ef3a4
xia1997x <48220443+harukodi@users.noreply.github.com>
2023-08-13 16:57:10 +0200

    Adding scripts for saltstack learning

---
+++ saltstack-master-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# Add saltstack repo
+sudo curl -fsSL -o /etc/apt/keyrings/salt-archive-keyring-2023.gpg [https://repo.saltproject.io/salt/py3/ubuntu/22.04/amd64/SALT-PROJECT-GPG-PUBKEY-2023.gpg](https://repo.saltproject.io/salt/py3/ubuntu/22.04/amd64/SALT-PROJECT-GPG-PUBKEY-2023.gpg)
+echo "deb [signed-by=/etc/apt/keyrings/salt-archive-keyring-2023.gpg arch=amd64] [https://repo.saltproject.io/salt/py3/ubuntu/22.04/amd64/latest](https://repo.saltproject.io/salt/py3/ubuntu/22.04/amd64/latest) jammy main" | sudo tee /etc/apt/sources.list.d/salt.list
+
+
+# Install saltstack-master and saltstack-ssh
+sudo apt-get update
+sudo apt-get install salt-master salt-ssh -y
+
+
+# Enable the salt-master service
+sudo systemctl enable salt-master
+sudo systemctl start salt-master
---
+++ saltstack-minion-install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+# Variable for the Ip of salt-master
+saltstack_master_ip=$1
+
+
+# Add saltstack repo
+sudo curl -fsSL -o /etc/apt/keyrings/salt-archive-keyring-2023.gpg [https://repo.saltproject.io/salt/py3/ubuntu/22.04/amd64/SALT-PROJECT-GPG-PUBKEY-2023.gpg](https://repo.saltproject.io/salt/py3/ubuntu/22.04/amd64/SALT-PROJECT-GPG-PUBKEY-2023.gpg)
+echo "deb [signed-by=/etc/apt/keyrings/salt-archive-keyring-2023.gpg arch=amd64] [https://repo.saltproject.io/salt/py3/ubuntu/22.04/amd64/latest](https://repo.saltproject.io/salt/py3/ubuntu/22.04/amd64/latest) jammy main" | sudo tee /etc/apt/sources.list.d/salt.list
+
+
+# Install saltstack-master and saltstack-ssh
+sudo apt-get update
+sudo apt-get install salt-minion salt-ssh -y
+
+
+# Echo the Ip for the salt-master
+echo "master: $saltstack_master_ip" >> /etc/salt/minion.d/master.conf
+
+
+# Enable the salt-minion service
+sudo systemctl enable salt-minion
+sudo systemctl start salt-minion
+
