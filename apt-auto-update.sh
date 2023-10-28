6e4e2f88da1c287e24d8f48f8b570dbb74734e91
xia1997x <48220443+harukodi@users.noreply.github.com>
2023-10-28 23:29:54 +0200

    Create apt-auto-update.sh

---
+++ apt-auto-update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+fetch_time=$(date "+%a/%T")
+pushover_user_key="user-token-goes-here"
+push_over_app_token="app-token-goes-here"
+
+if $(nc -zw 5 www.google.se 443); then
+    sudo NEEDRESTART_MODE=a apt-get update -y
+    sudo NEEDRESTART_MODE=a apt-get upgrade -y
+    # Update notification for pushover
+    curl -s -o /dev/null \
+      --form-string "token=$push_over_app_token" \
+      --form-string "user=$pushover_user_key" \
+      --form-string "message=emby-server was updated at $fetch_time" \
+      https://api.pushover.net/1/messages.json
+else
+    exit 1
+fi
