bffaca9818c09ac8fa7d85f4f15c3e4f547f5d98
None <nikolamihov2@gmail.com>
2025-03-09 16:05:17 +0100

    Update install_tailscale.sh

--- install_tailscale.sh
+++ install_tailscale.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 tailscale_key=$1
 
+ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N ""
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
 sudo apt-get update && apt-get install tailscale -y
